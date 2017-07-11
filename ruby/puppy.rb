d8aa74ef476e912a90cd5345613491159de788e7
Maria Jennings <mejennin@gmail.com>
2017-07-11 15:43:59 -0700

    Add comments regarding puppy class characterisitcs

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES ------
+# Puppy (Canis lupus familiaris junior)
+
+# CHARACTERISTICS -----
+# Friendly: yes
+# Fluffy: yes
+# Color: varies
+# Weight: Under 50 lbs
+
+# BEHAVIOR -----
+# Wags tail
+# Pees inside
+# Licks your face
+
